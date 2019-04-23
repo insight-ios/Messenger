fa32c285c55497150731ff0af71122c3b63ec66d
hongjuyeon <coke1317@naver.com>
2019-04-23 17:26:26 +0900

    FriendListViewController 클래스명 오타 수정 스토리보드 수정 및 DefaultUserListCell, FriendListCell 파일 추가 + 새 폴더 생성

---
+++ Messenger/FriendsTab/FriendsList/Cells/DefaultUserListCell.swift
@@ -0,0 +1,16 @@
+//
+//  DefaultUserListCell.swift
+//  Messenger
+//
+//  Created by hongjuyeon_dev on 23/04/2019.
+//  Copyright © 2019 insightCell. All rights reserved.
+//
+
+import UIKit
+
+class DefaultUserListCell: UICollectionViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nickNameLabel: UILabel!
+    
+}
--- Messenger/FriendsTab/FriendsList/FriendsListViewController.swift
+++ Messenger/FriendsTab/FriendsList/FriendsListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FiriendsListViewController: UIViewController {
+class FriendsListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
