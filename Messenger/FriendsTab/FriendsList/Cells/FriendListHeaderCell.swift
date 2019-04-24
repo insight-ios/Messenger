b1636b5fcb15e23901a9fe3814003e4a2c2dab32
hongjuyeon <coke1317@naver.com>
2019-04-24 16:57:32 +0900

    FriendsListViewController 수정 reuseIdentifier	에러 수정중

---
+++ Messenger/FriendsTab/FriendsList/Cells/FriendListHeaderCell.swift
@@ -0,0 +1,14 @@
+//
+//  FriendListCell.swift
+//  Messenger
+//
+//  Created by hongjuyeon_dev on 23/04/2019.
+//  Copyright © 2019 insightCell. All rights reserved.
+//
+
+import UIKit
+
+class FriendListHeaderCell: UICollectionViewCell {
+    
+    @IBOutlet weak var numOfUsersFriendLabel: UILabel!
+}
--- Messenger/FriendsTab/FriendsList/FriendsListViewController.swift
+++ Messenger/FriendsTab/FriendsList/FriendsListViewController.swift
@@ -9,22 +9,58 @@
 import UIKit
 
 class FriendsListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var users: [User]? {
+        didSet{
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UserStorage.shared.allUsers(completion: { userItems in
+            self.users = userItems
+        })
     }
-    */
+    
+    static func create() -> FriendsListViewController {
+        let sb = UIStoryboard(name: "FriendsList", bundle: nil)
+        return sb.instantiateViewController(withIdentifier: "FriendsListVC") as! FriendsListViewController
+    }
+    
+}
 
+extension FriendsListViewController:  UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       
+        if (section == 0) { return 1 }
+
+        else if (section == 1) { return 1 }
+
+        else {
+            guard let users = users else {
+                return 0
+            }
+            return users.count
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let users = users?[indexPath.item]{
+            if(indexPath.section == 0){
+            if let userListCell = collectionView.dequeueReusableCell(withReuseIdentifier: "UserListCell", for: indexPath) as? UserListCell {
+                    userListCell.userItem = users
+                    userListCell.bind(memberID: "0000")
+                    return userListCell
+                }
+            }
+        }
+        return UICollectionViewCell()
+    }
 }
